9301c63acd5ad2b49af77019161fbab2580083dc
crysthiano <crysthianophp@gmail.com>
2013-12-07 02:05:17 -0200

    estudos doctrine 2

---
+++ module/SONUser/config/module.config.php
@@ -0,0 +1,42 @@
+<?php
+
+return array(
+    'router' => array(
+        'routes' => array(
+            'sonuser-home' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route' => '/user',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'SONUser\Controller',
+                        'controller' => 'Index',
+                        'action'     => 'index'
+                    )
+                )
+            )
+        )
+    ),
+    
+    'controllers' => array(
+        'invokables' => array(
+            'SONUser\Controller\Index' => 'SONUser\Controller\IndexController'
+        )
+    ),
+    
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'son-user/index/index' => __DIR__ . '/../view/son-user/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
