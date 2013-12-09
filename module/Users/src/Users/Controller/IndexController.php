b88629520ecca9f9432ac17dc581d143835bb51d
crysthiano <crysthianophp@gmail.com>
2013-12-09 01:30:56 -0200

    estudos zend framework 2

---
+++ module/Users/src/Users/Controller/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Users\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+    
+    public function registerAction()
+    {
+        $view = new ViewModel();
+        $view->setTemplate('users/index/new-user');
+        return $view;
+    }
+    
+    public function loginAction()
+    {
+        $view = new ViewModel();
+        $view->setTemplate('users/index/login');
+        return $view;
+    }
+}
